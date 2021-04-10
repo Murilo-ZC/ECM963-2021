2c5e260b5ef43163278dd544716ff3079f71f59d
Murilo Zanini de Carvalho <carvalho.zanini@gmail.com>
2021-04-10 14:27:21 -0300

    Update pagina_inicial.dart

--- aula06/viacep_webrequest/lib/screens/pagina_inicial.dart
+++ aula06/viacep_webrequest/lib/screens/pagina_inicial.dart
@@ -1,26 +1,49 @@
 import 'package:flutter/material.dart';
 import 'package:viacep_webrequest/helpers/network_helper.dart';
 import 'package:viacep_webrequest/models/viacep.dart';
+import 'package:viacep_webrequest/widgets/card_cep.dart';
 
-class PaginaInicial extends StatelessWidget {
+class PaginaInicial extends StatefulWidget {
+  @override
+  _PaginaInicialState createState() => _PaginaInicialState();
+}
+
+class _PaginaInicialState extends State<PaginaInicial> {
+  ViaCEP cep = null;
+  final textCepController = TextEditingController();
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text("Consulta CEP"),
       ),
-      body: TextButton(
-        onPressed: () async {
-          var networkHelper =
-              NetworkHelper(url: "https://viacep.com.br/ws/01001000/json/");
-          var resultados = await networkHelper.getData();
-          var cep = ViaCEP.fromJson(resultados);
-          print("${resultados}");
-          print("Dados do Objeto:");
-          print("CEP:${cep.cep}");
-          print("Logradouro:${cep.logradouro}");
-        },
-        child: Text("Pegar Dados do CEP!"),
+      body: Column(
+        children: [
+          TextField(
+            controller: textCepController,
+          ),
+          TextButton(
+            onPressed: () async {
+              try {
+                final networkHelper = NetworkHelper(
+                    url:
+                        "https://viacep.com.br/ws/${textCepController.text}/json/");
+                final resultados = await networkHelper.getData();
+                cep = ViaCEP.fromJson(resultados);
+                print("${resultados}");
+                print("Dados do Objeto:");
+                print("CEP:${cep.cep}");
+                print("Logradouro:${cep.logradouro}");
+                setState(() {});
+              } catch (exception) {
+                print("Algo deu errado!");
+              }
+            },
+            child: Text("Pegar Dados do CEP!"),
+          ),
+          CardCEP(cep),
+        ],
       ),
     );
   }
